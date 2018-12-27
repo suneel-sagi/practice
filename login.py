0f7ef255a42b53ebe7dbc306bed010cd58c11eed
suni278 <suneelkumarsagi@gmail.com>
2018-12-27 20:44:31 +0530

    login

---
+++ login.py
@@ -0,0 +1,8 @@
+name = input("Enter yuorname :")
+passord = input("Enter your passwword : ")
+if  name==passord:
+    print(name,"your login is sucess")
+else:
+    print("enter your passwd as your name")
+print("This  is  test login page")
+print("Bye")
